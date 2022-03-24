084202e5ff17aefbedabe1be925024c6b41e923d
tm-26 <sammut026@gmail.com>
2022-03-24 16:07:22 +0100

    cleaned up clusterCalculator

--- src/clusterCalculator.py
+++ src/clusterCalculator.py
@@ -1,14 +1,19 @@
-"""Original algorithms can be found in:
-https://www-m9.ma.tum.de/material/felix-klein/clustering/MethodenHierarchisches_Clustern_Beispiel.php
-
 """
+Original algorithms can be found in:
+https://www-m9.ma.tum.de/material/felix-klein/clustering/Methoden/Hierarchisches_Clustern_Beispiel.php
+"""
+
+
 import csv
+import numpy
 from decimal import Decimal, ROUND_HALF_UP
+from itertools import groupby
+
 
 if __name__ == "__main__":
 
     # Parameter Declaration
-    distanceMetric = "kl"  # Can be either "pad" or "kl" or "hellinger" or "tv"
+    distanceMetric = "tv"  # Can be either "pad" or "kl" or "hellinger" or "tv"
 
     # Variable Declaration
     distances = []
@@ -34,10 +39,10 @@ if __name__ == "__main__":
         for c in range(len(distances)):
             distances[c] = list(map(float, distances[c]))
 
-
-    smallestDistance = min([c for dataset in distances for c in dataset])
-
+    distancesNumpy = numpy.array(distances)
+    smallestDistance = numpy.min(distancesNumpy[numpy.nonzero(distancesNumpy)])
     c = 0
+
     while c <= 3:
         distances[c] = distances[c][4:]
         c += 1
@@ -49,11 +54,3 @@ if __name__ == "__main__":
         print("Using Single Link --> " + str(Decimal(Decimal(min([c for dataset in distances for c in dataset])).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))) + '%')
         print("Using Complete Link --> " + str(Decimal(Decimal(max([c for dataset in distances for c in dataset])).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))) + '%')
         print("Using Average Link --> " + str(Decimal(Decimal((sum([sum(i) for i in zip(*distances)]) / 20)).quantize(Decimal("0.01"),rounding=ROUND_HALF_UP))) + '%')
-
-    else:
-        print("Using Single Link --> " + str(Decimal(Decimal(((min([c for dataset in distances for c in dataset]) - smallestDistance) / abs(smallestDistance)) * 100).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))) + '%')
-        print("Using Complete Link --> " + str(Decimal(Decimal(((max([c for dataset in distances for c in dataset]) - smallestDistance) / abs(smallestDistance)) * 100).quantize(Decimal("0.01"),rounding=ROUND_HALF_UP))) + '%')
-        print("Using Average Link --> " + str(Decimal(Decimal((((sum([sum(i) for i in zip(*distances)]) / 20) - smallestDistance) / abs(smallestDistance)) * 100).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))) + '%')
-
-    print()
-    print()
